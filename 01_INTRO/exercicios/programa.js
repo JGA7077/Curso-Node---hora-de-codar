ba9564a52fb9ddb501fe42a8b717c136bc9954df
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-06 15:06:42 -0300

    21.Módulos Internos

---
+++ 01_INTRO/exercicios/programa.js
@@ -0,0 +1,4 @@
+const num1 = 5;
+const num2 = 2;
+
+console.log('soma ==>', num1 + num2);
---
+++ 02_FUNDAMENTOS/01_modulo_interno/index.js
@@ -0,0 +1,6 @@
+const meuModulo = require('./meu_modulo');
+const soma = meuModulo.soma
+const somaT = meuModulo.somaT
+
+soma(1, 2)
+somaT(1, 3)
