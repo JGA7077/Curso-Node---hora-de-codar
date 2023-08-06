6f1d49aa3415ea70498ec2c5477039cc20ac5dbc
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-06 16:03:08 -0300

    24.Ler argumentos da linha de comando

---
+++ 02_FUNDAMENTOS/04_ler_argumentos/index.js
@@ -0,0 +1,17 @@
+// nome
+
+console.log('process.argv ==>', process.argv);
+
+const args = process.argv.slice(2)
+
+console.log('args ==>', args);
+
+const nome = args[0].split("=")[1]
+
+console.log('nome ==>', nome);
+
+const idade = args[1].split("=")[1]
+
+console.log('idade ==>', idade);
+
+console.log(`O nome dele é ${nome} e ele tem ${idade} anos`);
