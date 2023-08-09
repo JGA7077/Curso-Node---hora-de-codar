6bfa8e79c21e6af3b9313c11b5323474b8312ff3
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-09 16:13:47 -0300

    41.Retornando HTML

---
+++ 03_CORE_MODULES/02_retornando_html/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const port = 3333
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Olá, este é meu primeiro server com HTML</h1>')
+})
+
+server.listen(port, () => {
+  console.log('Servidor rodando na porta ==>', port);
+})
