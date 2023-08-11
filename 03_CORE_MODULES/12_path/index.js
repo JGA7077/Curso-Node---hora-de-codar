fe8aed9a0466d826239ca115640ecf5302e42cd9
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-11 10:24:57 -0300

    52.Módulo path

---
+++ 03_CORE_MODULES/12_path/index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const customPath = '/relatorios/matheus/relatorio1.pdf'
+
+console.log('path.dirname(customPath) ==>', path.dirname(customPath));
+console.log('path.basename(customPath) ==>', path.basename(customPath));
+console.log('path.extname(customPath) ==>', path.extname(customPath));
