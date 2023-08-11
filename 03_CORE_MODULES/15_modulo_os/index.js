9d378587750704834f33ff27f7f8f40fd52fd007
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-11 15:55:43 -0300

    55.Módulo os

---
+++ 03_CORE_MODULES/15_modulo_os/index.js
@@ -0,0 +1,6 @@
+const os = require('os')
+
+console.log('os.cpus() ==>', os.cpus());
+console.log('os.freemem() ==>', os.freemem());
+console.log('os.homedir() ==>', os.homedir());
+console.log('os.type() ==>', os.type());
