8598b7b8933f921f55557cc65e1236fb931e4b2e
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-11 11:05:23 -0300

    53.Path absoluto

---
+++ 03_CORE_MODULES/13_path_absoluto/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+// path absoluto
+console.log('path.resolve("teste.txt") ==>', path.resolve("teste.txt"));
+
+// formar path
+const midFolder = 'relatorios'
+const fileName = 'matheus.txt'
+
+const finalPath = path.join("/", "arquivos", midFolder, fileName)
+console.log('finalPath ==>', finalPath);
