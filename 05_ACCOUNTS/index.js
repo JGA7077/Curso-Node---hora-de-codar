860c8d9b3ce941dde71ccdc661bbf9a80569e268
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-12 16:39:20 -0300

    74.Finalizando criação de conta

--- 05_ACCOUNTS/index.js
+++ 05_ACCOUNTS/index.js
@@ -41,4 +41,43 @@ function operations() {
 function createAccount() {
   console.log(chalk.bgGreen.black('Parabéns por escolha o nosso banco!'));
   console.log(chalk.green('Defina as opções da sua conta a seguir'));
+
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer.prompt([
+    {
+      name: 'accountName',
+      message: 'Digite um nome para a sua conta:'
+    }
+  ])
+  .then((answer) => {
+    const {accountName} = answer;
+    console.info(accountName);
+
+    if (!fs.existsSync('accounts')) {
+      fs.mkdirSync('accounts')
+    }
+
+    if (fs.existsSync(`accounts/${accountName}.json`)) {
+      console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'));
+      buildAccount();
+      return;
+    }
+
+    fs.writeFileSync(
+      `accounts/${accountName}.json`, 
+      '{"balance": 0}', 
+      (err) => {
+        console.log('err ==>', err);
+      }
+    )
+
+    console.log(chalk.green('Parabéns, a sua conta foi criada!'));
+    operations();
+  })
+  .catch((err) => {
+    console.log('err ==>', err);
+  })
 }
