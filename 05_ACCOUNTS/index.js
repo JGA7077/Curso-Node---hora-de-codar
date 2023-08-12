f33b33ee72a8408cff87375ea6130f4d96cebe65
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-12 18:03:01 -0300

    77.Adição de saldo

--- 05_ACCOUNTS/index.js
+++ 05_ACCOUNTS/index.js
@@ -109,6 +109,21 @@ function deposit() {
     if (!checkAccount(accountName)) {
       return deposit();
     }
+
+    inquirer.prompt([
+      {
+        name: 'amount',
+        message: 'Quanto você deseja depositar?'
+      }
+    ])
+    .then((amountAnswer) => {
+      const {amount} = amountAnswer;
+
+      // add an amount
+      addAmount(accountName, amount);
+
+      operations();
+    })
   })
   .catch((err) => {
     console.log('err ==>', err);
@@ -124,4 +139,34 @@ function checkAccount(accountName) {
   }
 
   return true;
+}
+
+function addAmount(accountName, amount) {
+  const accountData = getAccount(accountName)
+
+  if (!amount) {
+    console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'));
+    return deposit();
+  }
+
+  accountData.balance = parseFloat(amount) + parseFloat(accountData.balance);
+
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    (err) => {
+      console.log('err ==>', err);
+    }
+  );
+
+  console.log(chalk.green(`Foi depositado o valor de R$${amount} na sua conta!`));
+}
+
+function getAccount(accountName) {
+  const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+    encoding: 'utf-8',
+    flag: 'r'
+  })
+
+  return JSON.parse(accountJSON)
 }
