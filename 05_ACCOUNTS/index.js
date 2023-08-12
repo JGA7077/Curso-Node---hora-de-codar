2cfc088c90a6a114cb591c72cbf2137c6abe9c5b
João Gabriel <53381109+JGA7077@users.noreply.github.com>
2023-08-12 16:05:55 -0300

    72.Inicialização do Sistema

--- 05_ACCOUNTS/index.js
+++ 05_ACCOUNTS/index.js
@@ -5,4 +5,27 @@ const chalk = require('chalk')
 // modulos internos
 const fs = require('fs')
 
-console.log('iniciamos o Accounts');
+operations()
+
+function operations() {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'actions',
+    message: 'O que você deseja fazer?',
+    choices: [
+      'Criar conta',
+      'Consultar Saldo',
+      'Depositar',
+      'Sacar',
+      'Sair'
+    ]
+  }])
+  .then((answer) => {
+    const {actions} = answer;
+
+    console.log('actions ==>', actions);
+  })
+  .catch((err) => {
+    console.log('err ==>', err);
+  })
+}
