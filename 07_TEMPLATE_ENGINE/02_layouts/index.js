071967a4a0d3f7470bbff4b6f2e43d152efe0127
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-15 21:27:33 -0300

    100.Criando Layouts

---
+++ 07_TEMPLATE_ENGINE/02_layouts/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('home')
+});
+
+app.listen(3333)
