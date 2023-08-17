3d12f662a0548e039345671802551f1e599d80e5
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-16 21:51:16 -0300

    106.Conhecendo os partials

--- 07_TEMPLATE_ENGINE/04_condicao/index.js
+++ 07_TEMPLATE_ENGINE/04_condicao/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
+const hbs = exphbs.create({
+  partialsDir: ['views/partials']
+})
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.get('/dashboard', (req, res) => {
@@ -34,6 +37,31 @@ app.get('/post', (req, res) => {
   }
 
   res.render('blogpost', {post})
+})
+
+app.get('/blog', (req, res) => {
+  const posts = [
+    {
+      title: 'Aprender Node.js',
+      category: 'Javascript',
+      body: 'Este artigo vai te ajudar a aprender Node.js.....',
+      comments: 4
+    },
+    {
+      title: 'Aprender Javascript',
+      category: 'Javascript',
+      body: 'Teste',
+      comments: 4
+    },
+    {
+      title: 'Aprender PHP',
+      category: 'PHP',
+      body: 'Teste',
+      comments: 4
+    }
+  ]
+
+  res.render('blog', {posts})
 })
 
 app.get('/', (req, res) => {
