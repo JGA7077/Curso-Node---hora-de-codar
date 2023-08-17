1c2b132bc9afddad0d71f2d1f182f0812a7fb3c3
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-17 20:05:27 -0300

    107.CSS com Handlebars

--- 07_TEMPLATE_ENGINE/04_condicao/index.js
+++ 07_TEMPLATE_ENGINE/04_condicao/index.js
@@ -8,6 +8,8 @@ const hbs = exphbs.create({
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static('public'))
+
 app.get('/dashboard', (req, res) => {
 
   const productItems = [
