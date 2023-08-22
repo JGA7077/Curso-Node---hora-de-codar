16b655c254d05f0347ac6ec679cb8eab982d1fad
João Gabriel <joaogabriel-al@hotmail.com>
2023-08-22 09:17:21 -0300

    119.Inserindo dados no banco

--- 08_NODE_E_MYSQL/01_instalacao_mysql/index.js
+++ 08_NODE_E_MYSQL/01_instalacao_mysql/index.js
@@ -4,10 +4,32 @@ const mysql = require('mysql');
 
 const app = express();
 
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+app.use(express.json())
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
+
+app.post('/books/insertbook', (req, res) => {
+  const {title, pagesqty} = req.body;
+  const sql = `INSERT INTO books (title, pagesqty) VALUES ('${title}', '${pagesqty}')`
+
+  conn.query(sql, function(err) {
+    if (err) {
+      console.error('Error on Insert Data', err)
+      return;
+    };
+
+    res.redirect('/')
+  });
+})
 
 app.get('/', (req, res) => {
   res.render('home')
